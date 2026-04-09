136369a9ae2b87a69abf975c6566bd0a059decfd
DND72 <ing.dellanoce@gmail.com>
2026-04-09 15:29:52 +0200

    fix

--- src/pages/dashboard/BiWheelPage.tsx
+++ src/pages/dashboard/BiWheelPage.tsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { useUser } from '@clerk/clerk-react'
 import ClientLayout from '../../components/dashboard/ClientLayout'
